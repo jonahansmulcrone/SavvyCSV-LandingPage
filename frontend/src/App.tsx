61734bae6fd9eb8fcb8984de6d080306c2bfdf37
jonahansmulcrone <126010204+jonahansmulcrone@users.noreply.github.com>
2024-10-05 19:24:25 -0700

    Added rotation to 3D Text Object, adjusted pivot origon to center of text as to make the text spin in-place.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,37 +1,37 @@
 import './App.css'
 import LandingPage from './LandingPage/LandingPage'
 
-function App() {
+const App = () => {
   return (
-    <div className='app-container'>
-      <div className='header'>
-        <div className='onyx-group'>
-          OnyxAI LLC
-        </div>
-        <div className='button-group'>
-          <button>Sign In</button>
-          <button>Create an Account</button>
-        </div>
-      </div>
-      <div className='content-container'>
-        <div className='content-column'>
-          <div className='content-title'>
-            <span className='accent-text'>Data-Driven</span> Solutions
+    <div className='main-container'>
+      <div className='main-wrapper'>
+        <div className='header'>
+          <div className='onyx-group'>
+            OnyxAI LLC
           </div>
-
-          <div className='spacer'>
-            
-          </div>
-
-          <div className='content-body'>
-            SavvyCSV leverages the power of AI and web scraping to create the perfect CSV file for your needs.
+          <div className='button-group'>
+            <button>Sign up</button>
           </div>
         </div>
-        <div className='savvy-logo-column' style={{ background: '#0D0D0D' }}>
-          <LandingPage />
+        <div className='content-container'>
+          <div className='content-column'>
+            <div className='content-title'>
+              <span className='accent-text'>Data-Driven</span> Solutions
+            </div>
+            <div className='content-body'>
+              SavvyCSV leverages the power of AI and web scraping to create the perfect CSV file for your needs.
+            </div>
+            <div className='start-button-container'>
+              <button>
+                Start now
+              </button>
+            </div>
           </div>
-        
-        <div className='logo-spacer'>
+          <div className='savvy-logo-column' style={{ background: '#0D0D0D', color: 'white' }}>
+            <div className='savvy-logo'>
+              <LandingPage />
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- frontend/src/LandingPage/LandingPage.tsx
+++ frontend/src/LandingPage/LandingPage.tsx
@@ -1,31 +1,54 @@
 import { RGBELoader } from 'three-stdlib'
-import { Canvas, useLoader } from '@react-three/fiber'
+import { Canvas, useFrame, useLoader } from '@react-three/fiber'
 import {
   Center,
   Text3D,
   Environment,
   Lightformer,
   OrbitControls,
-  RandomizedLight,
-  AccumulativeShadows,
   MeshTransmissionMaterial
 } from '@react-three/drei'
+import { useEffect, useRef } from 'react';
+import { Mesh, Vector3 } from 'three';
 
 interface TextProps {
-    children: React.ReactNode;
-    config: Record<string, any>; 
-    font?: string; 
-    [key: string]: any; 
-  }
+  children: React.ReactNode;
+  config: Record<string, any>;
+  font?: string;
+  [key: string]: any;
+}
 
 const Text: React.FC<TextProps> = ({ children, config, font = 'fonts/helvetiker_regular.typeface.json', ...props }) => {
   const texture = useLoader(RGBELoader, 'src/assets/pink_sunrise_4k.hdr')
+  const ref = useRef<Mesh>(null)
+
+  useEffect(() => {
+    if (ref.current) {
+      ref.current.geometry.computeBoundingBox();
+      const boundingBox = ref.current.geometry.boundingBox;
+      const center = new Vector3();
+
+      if (boundingBox) {
+        boundingBox.getCenter(center);
+      }
+
+      ref.current.geometry.translate(-center.x, -center.y, -center.z);
+      ref.current.position.set(15, 0, 0)
+    }
+  })
+
+  useFrame((state, delta) => {
+    if (ref.current) {
+      ref.current.rotation.y += delta
+    }
+  })
+
   return (
     <>
       <group>
-        <Center scale={[1, 1, 1]} top bottom {...props}>
+        <Center position={[0, 1, 0]} rotation={[-Math.PI / 12, Math.PI / 12, 0]}>
           <Text3D
-            castShadow
+            ref={ref}
             bevelEnabled
             font={font}
             scale={5}
@@ -44,50 +67,52 @@ const Text: React.FC<TextProps> = ({ children, config, font = 'fonts/helvetiker_
 }
 
 const SavvyLogo: React.FC = () => {
-    const text = 'SavvyCSV';
-    const config = {
-      backside: true,
-      backsideThickness: 0.15,
-      samples: 16,
-      resolution: 1024,
-      transmission: 1,
-      clearcoat: 1,
-      clearcoatRoughness: 0.0,
-      thickness: 0.3,
-      chromaticAberration: 0.15,
-      anisotropy: 0.25,
-      roughness: 0,
-      distortion: 10,
-      distortionScale: 0.1,
-      temporalDistortion: 0.60,
-      ior: 1.02,
-      color: '',
-      shadow: '#0D0D0D',
-    };
+  const text = 'SavvyCSV';
+  const config = {
+    backside: true,
+    backsideThickness: 0.3,
+    samples: 8,
+    resolution: 1024,
+    transmission: 0.8,
+    clearcoat: 1,
+    clearcoatRoughness: 0.0,
+    thickness: 0.3,
+    chromaticAberration: 5,
+    anisotropy: 0.3,
+    roughness: 0,
+    distortion: 0.5,
+    distortionScale: 0.1,
+    temporalDistortion: 1.2,
+    ior: 1.5,
+    color: '#ffffff',
+  };
 
   return (
     <Canvas shadows orthographic camera={{ position: [0, 10, 5], zoom: 18 }} >
-      <color attach="background" args={['#0D0D0D']}/>
+      <color attach="background" args={['#0D0D0D']} />
       <Text config={config} rotation={[150, 0, 0]} position={[0, 0, 5]}>
         {text}
       </Text>
       <OrbitControls
         autoRotateSpeed={-0.1}
         zoomSpeed={0.25}
-        minZoom={20}
-        maxZoom={20}
+        minZoom={18}
+        maxZoom={18}
         enablePan={false}
         dampingFactor={0.01}
-        minPolarAngle={Math.PI / 3}
-        maxPolarAngle={Math.PI / 3}
+        minPolarAngle={Math.PI / 2.5}
+        maxPolarAngle={Math.PI / 2.5}
       />
       <Environment resolution={32}>
         <group rotation={[-Math.PI / 4, -0.3, 0]}>
-        <Lightformer intensity={20} rotation-x={Math.PI / 2} position={[0, 5, -9]} scale={[10, 10, 1]} />
-          <Lightformer intensity={2} rotation-y={Math.PI / 2} position={[-5, 1, -1]} scale={[10, 2, 1]} />
-          <Lightformer intensity={2} rotation-y={Math.PI / 2} position={[-5, -1, -1]} scale={[10, 2, 1]} />
+          <Lightformer intensity={100} rotation-x={Math.PI / 2} position={[0, 5, -9]} scale={[10, 10, 1]} />
+          <Lightformer intensity={25} rotation-y={Math.PI / 2} position={[-5, 1, -1]} scale={[10, 2, 1]} />
+          <Lightformer intensity={25} rotation-y={Math.PI / 2} position={[-5, -1, -1]} scale={[10, 2, 1]} />
           <Lightformer intensity={2} rotation-y={-Math.PI / 2} position={[10, 1, 0]} scale={[20, 2, 1]} />
           <Lightformer type="ring" intensity={2} rotation-y={Math.PI / 2} position={[-0.1, -1, -5]} scale={10} />
+          <Lightformer intensity={4} rotation-y={Math.PI / 2} position={[-5, 1, -1]} scale={[20, 0.1, 1]} />
+          <Lightformer rotation-y={Math.PI / 2} position={[-5, -1, -1]} scale={[20, 0.5, 1]} />
+          <Lightformer rotation-y={-Math.PI / 2} position={[10, 1, 0]} scale={[20, 1, 1]} />
         </group>
       </Environment>
     </Canvas>
